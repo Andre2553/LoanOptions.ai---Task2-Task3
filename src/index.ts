d9ebade72370a786c25613bf0f14f9fa8bcceae5
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-11-01 22:56:57 +1100

    Add Args Handler & handle limit func

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ interface IApiDetails {
    Link: string;
    Category: string;
 }
-
 interface IApiResponse {
    data: {
       entries: IApiDetails[],
@@ -17,13 +16,44 @@ interface IApiResponse {
    }
 }
 
+interface IArgs {
+   category: string;
+   limit?: number;
+}
+
+function handleLimit(alldata: IApiDetails[]) {
+   if (limit) {
+      if (limit > alldata.length) {
+         console.log(`Limit is greater than the number of APIs available. Showing all APIs.`);
+         alldata = alldata;
+      } else {
+         alldata = alldata.slice(0, limit);
+      }
+   }
+   return alldata;
+
+}
+
+//Get the arguments from the command line and create the IArgs object
+const args = process.argv.slice(2);
+const category = args[0];
+const limit = args[1] ? parseInt(args[1]) : 10;
+
+const apiArgs: IArgs = { category, limit };
+
+let alldata: IApiDetails[] = [];
+
 // API URL and Config for Axios
 const config = {
    method: 'get',
-   url: "https://api.publicapis.org/entries",
+   url: `https://api.publicapis.org/entries${apiArgs.category ? `?category=${apiArgs.category}` : ''}`,
 }
 axios(config)
    .then((response: IApiResponse) => {
-      const { entries, count } = response.data;
-      console.log(entries, count);
+      const { entries } = response.data;
+      alldata = entries;
+      alldata = handleLimit(alldata);
+      (alldata.reverse()).forEach((api) => {
+         console.log(api.API);
+      });
    })
